6f6d0f60a12dda2aac00b7edbe275987d275e6a3
Florian Hegg <florian.hegg@hftm.ch>
2023-10-15 11:45:34 +0200

    work in progress

--- pages/about.php
+++ pages/about.php
@@ -1,1 +1,44 @@
 <h1>Über Uns</h1>
+
+<div class="gradient-top"></div>
+<div class="bg">
+    <div id="carousel" class="carousel slide">
+        <div class="gradient-top"></div>
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="./img/background1.jpg" style="margin-top: 0px;">
+            </div>
+            <div class="item">
+                <img src="./img/background2.jpg" style="margin-top: 0px;">
+            </div>
+            <div class="item">
+                <img src="./img/background3.jpg" style="margin-top: 0px;">
+            </div>
+        </div>
+        <div class="gradient"></div>
+    </div>
+</div>
+
+<div class="tab-content">
+    <div class="tab-content">
+        <div class="row">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-2">
+                <img class="img-fluid" src="./img/flavio.png" alt="Flavio">
+                <p>Als langjähriger Schlagzeuger, Militärmusikant und Person die es generell preferiert 'wenns chli
+                    chlefelet' Hat sich Flavio Lang seit Jahren um die entstehung von NotenNest bemüht und ist ein
+                    wertvolles Gründungsmitglied der Firma.</p>
+            </div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-2">
+                <img class="img-fluid" src="./img/florian.jpeg" alt="Florian">
+                <p>Flo ist durch seine anfängliche DJ karriere schon früh dafür bekannt gewesen zu viel Geld für
+                    modernens Soundequpment zu verlieren. Die idee das Problem mittels einer eigenen Firma zu bewältigen
+                    hat ihm also in die Karten gespielt.</p>
+            </div>
+            <div class="col-sm-3">
+            </div>
+        </div>
+    </div>
+</div>
+</div>
--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,39 @@
-<h1 class="m-auto p-auto">Hi <?php echo getUrlParamOrEmptyString('name') ?></h1>
+<head>
+    <meta charset="utf-8">
+    <title>NotenNest</title>
+
+    <link href="/NotenNest/css/home.css" rel="stylesheet">
+</head>
+
+<h1 class="m-auto p-auto">
+    <?php echo getUrlParamOrEmptyString('name') ?>
+</h1>
+
+<div class="tab-content">
+    <br>
+    <div class="col-sm-12">
+        <div class="row">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-2">
+                <img class="img-fluid" src="./img/flavio.png" alt="Flavio">
+            </div>
+            <div class="col-sm-1"></div>
+            <div class="col-sm-2">
+                <img class="img-fluid" src="./img/flavio.png" alt="Flavio">
+            </div>
+            <div class="col-sm-1"></div>
+            <div class="col-sm-2">
+                <img class="img-fluid" src="./img/flavio.png" alt="Flavio">
+            </div>
+            <div class="col-sm-2"></div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
+                <p>NotenNest, ihr kompetenter Partener für </p>
+            </div>
+            <div class="col-sm-2"></div>
+        </div>
+    </div>
+</div>

696b096666ea00dd32f8af4b267ac192dd4caac4
Florian Hegg <florian.hegg@hftm.ch>
2023-10-15 21:13:54 +0200

    small change

--- pages/home.php
+++ pages/home.php
@@ -10,7 +10,7 @@
 </h1>
 <style>
     body {
-        background-image: linear-gradient(0deg, rgba(215, 215, 217, 1) 10%, rgba(217, 215, 216, 0) 100%), url('./img/background3.jpg');
+        background-image: linear-gradient(0deg, rgba(215, 215, 217, 1) 20%, rgba(217, 215, 216, 0) 100%), url('./img/background3.jpg');
         background-repeat: no-repeat;
     }
 </style>

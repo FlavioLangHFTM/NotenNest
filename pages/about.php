9484e83ba34e7c451f76b9ac0fd50f49c5e0e0b3
Florian Hegg <florian.hegg@hftm.ch>
2023-10-15 21:12:31 +0200

    fixed image

--- pages/about.php
+++ pages/about.php
@@ -1,71 +1,68 @@
-<body>
-    <style>
-        body {
-            background-image: linear-gradient(0deg, rgba(215, 215, 217, 1) 20%, rgba(217, 215, 216, 0) 100%), url('./img/background3.jpg');
-            background-repeat: no-repeat;
-        }
-    </style>
-    <div class="col-sm-12" align="center">
-        <br>
-        <h1 color="white">Über die Firma</h1>
-        <br>
-        <div class="row" align="justify">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-8">
-                <p>
-                    Flavio und Florian waren enge Freunde, die eine tiefe Liebe zur Musik teilten. Flavio spielte
-                    Schlagzeug und Florian die Trompete. Als sie feststellten, dass viele Menschen aufgrund der hohen
-                    Kosten
-                    keine Musikinstrumente erlernen konnten, beschlossen sie, etwas dagegen zu unternehmen. <br><br>
+<style>
+    body {
+        background-image: linear-gradient(0deg, rgba(215, 215, 217, 1) 20%, rgba(217, 215, 216, 0) 100%), url('./img/background3.jpg');
+        background-repeat: no-repeat;
+    }
+</style>
+<div class="col-sm-12" align="center">
+    <br>
+    <h1 color="white">Über die Firma</h1>
+    <br>
+    <div class="row" align="justify">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+            <p>
+                Flavio und Florian waren enge Freunde, die eine tiefe Liebe zur Musik teilten. Flavio spielte
+                Schlagzeug und Florian die Trompete. Als sie feststellten, dass viele Menschen aufgrund der hohen
+                Kosten
+                keine Musikinstrumente erlernen konnten, beschlossen sie, etwas dagegen zu unternehmen. <br><br>
 
-                    Im Jahr 1995 gründeten Flavio und Florian "NotenNest". Sie sammelten gebrauchte Instrumente von der
-                    Gemeinschaft und ermöglichten es den Menschen, sie auszuleihen. Zudem boten sie kostenlosen
-                    Unterricht an, um die Freude an der Musik zu teilen.<br><br>
+                Im Jahr 1995 gründeten Flavio und Florian "NotenNest". Sie sammelten gebrauchte Instrumente von der
+                Gemeinschaft und ermöglichten es den Menschen, sie auszuleihen. Zudem boten sie kostenlosen
+                Unterricht an, um die Freude an der Musik zu teilen.<br><br>
 
-                    Flavio unterrichtete Schlagzeug und Florian die Trompete. Mit der Zeit wuchs NotenNest und
-                    erweiterte
-                    sein Angebot, um Menschen jeden Alters und jeder Fähigkeitsstufe zu unterstützen. Die Geschichte von
-                    Flavio und Florian zeigt, wie zwei Freunde mit Leidenschaft und Entschlossenheit die Musik für viele
-                    zugänglich machten und Herzen berührten.
-                </p>
-            </div>
-            <div class="col-sm-2"></div>
+                Flavio unterrichtete Schlagzeug und Florian die Trompete. Mit der Zeit wuchs NotenNest und
+                erweiterte
+                sein Angebot, um Menschen jeden Alters und jeder Fähigkeitsstufe zu unterstützen. Die Geschichte von
+                Flavio und Florian zeigt, wie zwei Freunde mit Leidenschaft und Entschlossenheit die Musik für viele
+                zugänglich machten und Herzen berührten.
+            </p>
         </div>
-        <br>
-        <br>
+        <div class="col-sm-2"></div>
+    </div>
+    <br>
+    <br>
 
-        <h1>Gründungsmitglieder</h1>
-        <br>
+    <h1>Gründungsmitglieder</h1>
+    <br>
+    <div class="tab-content">
         <div class="tab-content">
-            <div class="tab-content">
-                <div class="row">
-                    <div class="col-sm-3"></div>
-                    <div class="col-sm-2">
-                        <img class="img-fluid" src="./img/flavio.png" alt="Flavio"></img>
-                        <br>
-                        <br>
-                        <p align="justify">Als langjähriger Schlagzeuger, Militärmusikant und Person, die es generell
-                            präferiert 'wenns chli
-                            chlefelet' hat sich Flavio Lang seit Jahren um die Entstehung von NotenNest bemüht und ist
-                            ein
-                            wertvolles Gründungsmitglied der Firma.</p>
-                    </div>
-                    <div class="col-sm-2"></div>
-                    <div class="col-sm-2">
-                        <img class="img-fluid" src="./img/florian.jpeg" alt="Florian"></img>
-                        <br>
-                        <br>
-                        <p align="justify">FFlo ist durch seine anfängliche DJ-Karriere schon früh dafür bekannt gewesen
-                            zu
-                            viel Geld für
-                            modernens Soundequipment auszugeben. Die Idee, das Problem mittels einer eigenen Firma zu
-                            bewältigen
-                            hat ihm also in die Karten gespielt.</p>
-                    </div>
-                    <div class="col-sm-3">
-                    </div>
+            <div class="row">
+                <div class="col-sm-3"></div>
+                <div class="col-sm-2">
+                    <img class="img-fluid" src="./img/flavio.png" alt="Flavio"></img>
+                    <br>
+                    <br>
+                    <p align="justify">Als langjähriger Schlagzeuger, Militärmusikant und Person, die es generell
+                        präferiert 'wenns chli
+                        chlefelet' hat sich Flavio Lang seit Jahren um die Entstehung von NotenNest bemüht und ist
+                        ein
+                        wertvolles Gründungsmitglied der Firma.</p>
                 </div>
+                <div class="col-sm-2"></div>
+                <div class="col-sm-2">
+                    <img class="img-fluid" src="./img/florian.jpeg" alt="Florian"></img>
+                    <br>
+                    <br>
+                    <p align="justify">Flo ist durch seine anfängliche DJ-Karriere schon früh dafür bekannt gewesen
+                        zu
+                        viel Geld für
+                        modernens Soundequipment auszugeben. Die Idee, das Problem mittels einer eigenen Firma zu
+                        bewältigen
+                        hat ihm also in die Karten gespielt.</p>
+                </div>
+                <div class="col-sm-3"></div>
             </div>
         </div>
     </div>
-</body>
+</div>
--- pages/home.php
+++ pages/home.php
@@ -8,61 +8,58 @@
 <h1 class="m-auto p-auto">
     <?php echo getUrlParamOrEmptyString('name') ?>
 </h1>
-
-<body>
-    <style>
-        body {
-            background-image: linear-gradient(0deg, rgba(215, 215, 217, 1) 10%, rgba(217, 215, 216, 0) 100%), url('./img/background3.jpg');
-            background-repeat: no-repeat;
-        }
-    </style>
-    <br>
-    <div class="col-sm-12" align="justify">
-        <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-2">
-                <img class="img-fluid" box-shadow=2px src="./img/guitar.jpg" alt="Flavio">
-            </div>
-            <div class="col-sm-1"></div>
-            <div class="col-sm-2">
-                <img class="img-fluid" src="./img/keyboard.jpg" alt="Flavio">
-            </div>
-            <div class="col-sm-1"></div>
-            <div class="col-sm-2">
-                <img class="img-fluid" src="./img/ukulele.jpg" alt="Flavio">
-            </div>
-            <div class="col-sm-2"></div>
+<style>
+    body {
+        background-image: linear-gradient(0deg, rgba(215, 215, 217, 1) 10%, rgba(217, 215, 216, 0) 100%), url('./img/background3.jpg');
+        background-repeat: no-repeat;
+    }
+</style>
+<br>
+<div class="col-sm-12" align="justify">
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-2">
+            <img class="img-fluid" box-shadow=2px src="./img/guitar.jpg" alt="Flavio">
         </div>
-        <br>
-        <br>
-        <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-8">
-                <h1>NotenNest, hier läuft die Musik!</h1>
-                <h3>Instrumentenverleih der den Takt trifft.</h3>
-            </div>
-            <div class="col-sm-2"></div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-2">
+            <img class="img-fluid" src="./img/keyboard.jpg" alt="Flavio">
         </div>
-        <br>
-        <br>
-        <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-8">
-                <p>Lieferung vom 13. Oktober 2023, neue Produkte im Sortiment!
-                </p>
-            </div>
-            <div class="col-sm-2"></div>
-            <div class="col-sm-2"></div>
-            <div class="col-sm-8">
-                <p>Bei NotenNest sind wir stolz darauf, Ihre musikalischen Träume Wirklichkeit werden zu lassen.
-                    Egal, ob Sie ein erfahrener Profi oder ein aufstrebender Musikliebhaber sind, unser
-                    Instrumentenverleih-Service steht Ihnen mit einer breiten Auswahl an hochwertigen
-                    Instrumenten
-                    zur Verfügung.
-                </p>
-                <div class="col-sm-2"></div>
-            </div>
-            <div class="col-sm-2"></div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-2">
+            <img class="img-fluid" src="./img/ukulele.jpg" alt="Flavio">
         </div>
+        <div class="col-sm-2"></div>
     </div>
-</body>
+    <br>
+    <br>
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+            <h1>NotenNest, hier läuft die Musik!</h1>
+            <h3>Instrumentenverleih der den Takt trifft.</h3>
+        </div>
+        <div class="col-sm-2"></div>
+    </div>
+    <br>
+    <br>
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+            <p>Lieferung vom 13. Oktober 2023, neue Produkte im Sortiment!
+            </p>
+        </div>
+        <div class="col-sm-2"></div>
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+            <p>Bei NotenNest sind wir stolz darauf, Ihre musikalischen Träume Wirklichkeit werden zu lassen.
+                Egal, ob Sie ein erfahrener Profi oder ein aufstrebender Musikliebhaber sind, unser
+                Instrumentenverleih-Service steht Ihnen mit einer breiten Auswahl an hochwertigen
+                Instrumenten
+                zur Verfügung.
+            </p>
+            <div class="col-sm-2"></div>
+        </div>
+        <div class="col-sm-2"></div>
+    </div>
+</div>

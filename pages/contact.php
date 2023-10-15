dc4447000fa6b86f42502a6fec6301a6f494a3f5
Florian Hegg <florian.hegg@hftm.ch>
2023-10-15 22:04:03 +0200

    finished contact section

--- pages/contact.php
+++ pages/contact.php
@@ -1,5 +1,5 @@
 <head>
-    <link href="/NotenNest/css/home.css" rel="stylesheet">
+    <link href="/NotenNest/css/contact.css" rel="stylesheet">
 </head>
 
 <style>
@@ -10,3 +10,41 @@
 </style>
 <br>
 <h1>Kontaktformular</h1>
+
+<div class="col-sm-12">
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+            <form>
+                <div id="contact_body">
+                    <div>
+                        <label for="name"></label>
+                        <input type="text" name="name" id="name" required="true" placeholder="Name" />
+                    </div>
+
+                    <div>
+                        <label for="email"></label>
+                        <input type="email" name="email" required="true" placeholder="E-Mail" />
+                    </div>
+
+                    <div>
+                        <label for="subject"></label>
+                        <input type="text" name="subject" required="true" placeholder="Betrifft" />
+                    </div>
+
+                    <div class="form-kontakt-group">
+                        <label for="message"></label>
+                        <textarea name="message" id="message" required="true"
+                            placeholder="Nachricht eingeben"></textarea>
+                    </div>
+                </div>
+
+                <div class="text-center">
+                    <input type="submit" id="submit_btn" value="Nachricht senden">
+                </div>
+
+            </form>
+        </div>
+        <div class="col-sm-2"></div>
+    </div>
+</div>
